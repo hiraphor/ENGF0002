d03fc2a38d539a7e60280b4356de2acfcbcfbee2
hiraphor <43610014+hiraphor@users.noreply.github.com>
2018-11-22 01:37:07 +0000

    Update te_autoplayer.py

--- Assignments/assignment4/src/te_autoplayer.py
+++ Assignments/assignment4/src/te_autoplayer.py
@@ -13,7 +13,32 @@ class AutoPlayer():
             gamestate supplies access to all the state needed to autoplay the game.'''
         self.random_next_move(gamestate)
 
-    def random_next_move(self, gamestate):
+                
+    def search_all(self, gamestate):
+        left = Direction.LEFT
+        right = Direction.Right
+        x = 1
+        y = 1
+        
+        for i in range(16):
+            clone = gamestate.clone(True)
+                for j in range(x):
+                    clone.move(left)
+                    for k in range(y):
+                        clone.rotate()
+                        clone.update()
+                        if y<4:
+                            y+=1
+                        else:
+                            y=1
+                            x+=1       
+        
+        
+        
+        
+        
+        
+        
         ''' make a random move and a random rotation.  Not the best strategy! '''
         rnd = self.rand.randint(-1, 1)
         if rnd == -1:
